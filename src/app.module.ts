f3ed99290122e46d3773e89caed786390b701ae8
Krishna Prasad Khanal <krishnaamen@gmail.com>
2024-02-20 09:41:15 +0100

    generating the crud entrypoints for entries and its dtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EntriesModule } from './entries/entries.module';
 
 @Module({
-  imports: [],
+  imports: [EntriesModule],
   controllers: [AppController],
   providers: [AppService],
 })

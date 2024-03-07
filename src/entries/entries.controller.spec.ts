5a33fe55dc533d5c5b7c65454902b70eb0a41f0f
juaninicolai <juan.nicolai@maersk.com>
2024-03-07 16:08:50 +0100

    fix tests

--- src/entries/entries.controller.spec.ts
+++ src/entries/entries.controller.spec.ts
@@ -1,14 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EntriesController } from './entries.controller';
 import { EntriesService } from './entries.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Entry } from './entities/entry.entity';
 
 describe('EntriesController', () => {
   let controller: EntriesController;
 
   beforeEach(async () => {
+    const mockEntry = new Entry();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EntriesController],
-      providers: [EntriesService],
+      providers: [
+        EntriesService,
+        {
+          provide: getRepositoryToken(Entry),
+          useValue: {
+            save: jest.fn().mockResolvedValue(mockEntry),
+            find: jest.fn().mockResolvedValue([mockEntry]),
+            findOne: jest.fn().mockResolvedValue(mockEntry),
+            update: jest.fn().mockResolvedValue({}),
+            delete: jest.fn().mockResolvedValue({}),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<EntriesController>(EntriesController);
--- src/entries/entries.service.spec.ts
+++ src/entries/entries.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EntriesService } from './entries.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Entry } from './entities/entry.entity';
 
 describe('EntriesService', () => {
   let service: EntriesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EntriesService],
+      providers: [
+        EntriesService,
+        {
+          provide: getRepositoryToken(Entry),
+          useValue: {
+            save: jest.fn().mockResolvedValue({}),
+            find: jest.fn().mockResolvedValue([{}]),
+            findOne: jest.fn().mockResolvedValue({}),
+            update: jest.fn().mockResolvedValue({}),
+            delete: jest.fn().mockResolvedValue({}),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<EntriesService>(EntriesService);

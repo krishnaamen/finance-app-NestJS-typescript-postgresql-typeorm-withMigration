2ba5f5ee9d0ad894eed30514f8966db25ea7a79d
juaninicolai <juan.nicolai@maersk.com>
2024-03-07 16:48:10 +0100

    Add test

--- src/entries/entries.controller.spec.ts
+++ src/entries/entries.controller.spec.ts
@@ -4,8 +4,11 @@ import { EntriesService } from './entries.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Entry } from './entities/entry.entity';
 
+jest.mock('./entries.service');
+
 describe('EntriesController', () => {
   let controller: EntriesController;
+  let service: EntriesService;
 
   beforeEach(async () => {
     const mockEntry = new Entry();
@@ -18,18 +21,29 @@ describe('EntriesController', () => {
           useValue: {
             save: jest.fn().mockResolvedValue(mockEntry),
             find: jest.fn().mockResolvedValue([mockEntry]),
-            findOne: jest.fn().mockResolvedValue(mockEntry),
-            update: jest.fn().mockResolvedValue({}),
-            delete: jest.fn().mockResolvedValue({}),
           },
         },
       ],
     }).compile();
 
     controller = module.get<EntriesController>(EntriesController);
+    service = jest.mocked(controller.entriesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a new entry', async () => {
+    const newEntry = {
+      amount: 100,
+      currency: 'DKK',
+      date: new Date(),
+      category: 'Cinema',
+      name: 'Social',
+      comment: 'comment',
+    };
+    await controller.create(newEntry);
+    expect(service.create).toHaveBeenCalledWith(newEntry);
   });
 });
--- src/entries/entries.controller.ts
+++ src/entries/entries.controller.ts
@@ -13,7 +13,7 @@ import { UpdateEntryDto } from './dto/update-entry.dto';
 
 @Controller('entries')
 export class EntriesController {
-  constructor(private readonly entriesService: EntriesService) {}
+  constructor(readonly entriesService: EntriesService) {}
 
   @Post()
   create(@Body() createEntryDto: CreateEntryDto) {

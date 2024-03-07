9648a5b71ba1d8a7a7d08bcbae74d9c7689aaab6
Veselin Ivanov <me@veselyn.com>
2024-03-07 18:02:27 +0100

    remove categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { EntriesModule } from './entries/entries.module';
 import { dbConfig } from 'data.source';
-import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(dbConfig),
     EntriesModule,
-    CategoriesModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
